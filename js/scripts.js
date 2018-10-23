aca9db2d989f378000c92e86f129f98ce599caab
Christopher Cahill and Jacey Tupa <christopher@>
2018-10-23 16:24:01 -0700

    added two fields and custom delete

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,37 @@ function splitString(stringToSplit, separator) {
     return (stringToSplit.split(separator));
   }
 
-function vowelDelete(inputArray) {
+function letterDelete(inputArray, vowelArray) {
   var returnArray = [];
-  for (var i = 0; i <= inputArray.length-1; i++)
+  for (var i = 0; i <= (inputArray.length)-1; i++) {
+    if (vowelArray.includes(inputArray[i]))
+    {
+    returnArray.push("-")
+    }
+    else {returnArray.push(inputArray[i])
+    }
+  }
+  return returnArray;
 }
 
 $(document).ready(function() {
   $("form#formSubmit").submit(function(event) {
     event.preventDefault();
     var inputString = $("#stringInput").val();
+    var deleteString = $("#letterInput").val();
 
     var stringArray = splitString(inputString, "");
+    var deleteArray = splitString(deleteString, "")
 
-
+    var deletedLetters = letterDelete(stringArray, deleteArray);
 
     console.log(stringArray);
+    console.log(deleteArray);
+    console.log(deletedLetters);
 
   });
 
 });
 
 
-if ("aeiouAEIOU")
+// if ("aeiouAEIOU")
